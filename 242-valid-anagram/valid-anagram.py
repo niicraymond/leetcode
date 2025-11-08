7e6d66d47403f9ede1e2d447ef5438def9f7c7f2
niicraymond <nicoleraymond@hotmail.com>
2025-11-08 18:34:23 +0000

    Time: 12 ms (51.48%) | Memory: 17.7 MB (86.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+
+        return self.track_letters(s) == self.track_letters(t)
+
+    def track_letters(self, s: str) -> dict:
+        letters = {}
+        for char in s:
+            if char in letters:
+                letters[char] += 1
+            else:
+                letters[char] = 1
+        return letters
