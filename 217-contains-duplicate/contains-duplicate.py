ed6843f651bab1301c9733af7d046b9eda079f30
niicraymond <nicoleraymond@hotmail.com>
2025-11-11 17:38:34 +0000

    Time: 4 ms (96.74%) | Memory: 31.5 MB (53.81%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,19 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
-        store = {}
+                
+         return len(set(nums)) != len(nums)
 
-        for i, n in enumerate(nums):
-            if n in store:
-                return True                
-            else:
-                store[n] = i
-        return False        
-         
-
-        #create a hashmap to store in numbers
-            #loop and add each item to the hashmap
-                #if already in the hashmap
-                    #return true
-                #if the loop ends
-                    #return false    
+        #put nums into a set and see if the lengths are the same
+        #set will eliminate duplicates so if any duplicates it'll remove them
+        #therefore the lengths will be diffferent   
