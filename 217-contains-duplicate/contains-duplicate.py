a1d5a5718f2b25e2b2ff084bc1446416b81b4740
niicraymond <nicoleraymond@hotmail.com>
2025-11-11 17:32:16 +0000

    Time: 23 ms (20.34%) | Memory: 38.1 MB (10.15%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,10 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        store = {}
 
-        num_tracker = {}
-        for i in nums:
-            if i in num_tracker:
-                return True
-            num_tracker[i] = i
+        for i, n in enumerate(nums):
+            if n in store:
+                return True                
+            else:
+                store[n] = i
+        return False        
+         
 
-        return False
+        #create a hashmap to store in numbers
+            #loop and add each item to the hashmap
+                #if already in the hashmap
+                    #return true
+                #if the loop ends
+                    #return false    
